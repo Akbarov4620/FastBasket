c1d30dd115542db6542b9f004d319dc4567f23e3
Akbarov4620 <ogabekakbarov4620@gmail.com>
2023-09-16 17:13:02 +0500

    first commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Footer.scss'
+
+export default function Footer() {
+  return (
+    <footer className="site-footer">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8 col-sm-6 col-xs-12">
+            <p className="copyright-text">Copyright &copy; 2023 All Rights Reserved by
+              <a href="https://github.com/Akbarov4620">Og'abek</a>.
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
+}
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './Header.scss'
+import shop from '../../assets/shop.svg'
+import user from '../../assets/user.svg'
+import basket from '../../assets/basket.svg'
+
+export default function Header() {
+  return (
+    <>
+      <header>
+        <div className="container">
+          <div className="header">
+            <h1>
+              <a style={{display: 'flex', alignItems: 'center', gap: '10px'}} href="/">
+                <img src={basket} alt='header logo' width={50} height={50}/>
+                <span style={{color: 'black'}}>FastBasket</span>
+              </a>
+            </h1>
+            <div className="header__others-box">
+              <a href='/cart'>
+                <img src={shop} width={28} height={28} alt="shop svg" />
+              </a>
+              <a href='/profile'>
+                <img src={user} width={30} height={30} alt="user svg" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  )
+}
